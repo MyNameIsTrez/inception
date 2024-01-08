7f2dbbdbe6d76a5276ee7a34d8fccfb1119b1e41
MyNameIsTrez <welfje@gmail.com>
2024-01-08 10:09:29 -0500

    Fix /wordpress being added at the end of URL

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -14,17 +14,12 @@ if [ ! $? -eq 0 ]; then
 return 1
 fi
 
-# echo "Moving wordpress files to /var/www"
-# mv /var/www/wordpress/* /var/www
-# rm -rf /var/www/wordpress
-fi
-
 echo "Running wp core install"
 wp core install --title="$WP_TITLE" --admin_name="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --url="$WP_URL"
 
-# TODO: Move this to the Dockerfile
 echo "Running wp user create"
 wp user create "$WP_USER_NAME" "$WP_USER_EMAIL" --role=contributor --user_pass="$WP_USER_PASSWORD"
+fi
 
 # --nodaemonize forces staying in the foreground
 echo "Running php-fpm"
