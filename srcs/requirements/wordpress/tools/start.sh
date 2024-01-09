76f07759ed76514306264fec0c955eec1addd2a9
MyNameIsTrez <welfje@gmail.com>
2024-01-09 10:31:58 -0500

    Formatting

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -5,25 +5,31 @@ set -e
 
 if [ ! -f "/var/www/wp-config.php" ]; then
 echo "Creating config"
-wp-cli --allow-root --path=/var/www config create\
+wp-cli config create\
+	--allow-root\
+	--path=/var/www\
 	--dbhost="$DB_HOST"\
 	--dbname="$DB_NAME"\
 	--dbuser="$DB_USER"\
 	--dbpass="$DB_PASSWORD"
 
 echo "Running wp core install"
-wp-cli --allow-root --path=/var/www core install\
+wp-cli core install\
+	--allow-root\
+	--path=/var/www\
+	--skip-email\
 	--title="$WP_ADMIN_TITLE"\
 	--admin_name="$WP_ADMIN_NAME"\
 	--admin_password="$WP_ADMIN_PASSWORD"\
 	--admin_email="$WP_ADMIN_EMAIL"\
-	--skip-email\
 	--url="$WP_ADMIN_URL"
 
 echo "Running wp user create"
-wp-cli --allow-root --path=/var/www user create\
+wp-cli user create\
 	"$WP_USER_NAME"\
 	"$WP_USER_EMAIL"\
+	--allow-root\
+	--path=/var/www\
 	--role="$WP_USER_ROLE"\
 	--user_pass="$WP_USER_PASSWORD"
 fi
