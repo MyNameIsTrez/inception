45c2392e3f0364f792c6d3d7f2d0e30593977fb7
MyNameIsTrez <welfje@gmail.com>
2024-01-12 06:52:05 -0500

    Remove race condition-y and redundant healthcheck

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -3,11 +3,6 @@
 # This lets the script exit if any command fails
 set -e
 
-if [ -f "$HEALTHCHECK_FILE" ]; then
-	echo "Healthchech file already exists, removing it"
-	rm "$HEALTHCHECK_FILE"
-fi
-
 # Since /var/lib/mysql is the persistent volume,
 # we don't need to do this every time the container boots,
 # but this should really just be moved to the Dockerfile
@@ -24,12 +19,6 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 else
 	echo "mariadb database was already initialized"
 fi
-
-# We're creating this file after bootstrapping is done
-# to signal that the wordpress container can now start and use mariadb
-# The reason we don't need a healthcheck for wordpress is because nginx
-# will only access wordpress once a user requests a page
-touch "$HEALTHCHECK_FILE"
 
 echo "Running mariadbd"
 mariadbd
